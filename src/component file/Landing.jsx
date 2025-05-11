3ed00d93dc73a4ef80f4e529f969bb799b1e5e7b
Amer Mohamed <96571594+Amermob@users.noreply.github.com>
2025-05-11 12:10:02 +0300

    Update Landing.jsx

--- src/component file/Landing.jsx
+++ src/component file/Landing.jsx
@@ -2,7 +2,7 @@ export default function Header() {
   return (
     <div className="landing">
       <video loop={true} muted={true} autoPlay={true}>
-        <source src="landing-background.mp4" />
+        <source src="/landing-background.mp4" />
       </video>
       <div className="text-section">
         <h1>Blue Arrow Events</h1>
